de0eabc2e08509aca1f7a716ea7dafa139a4f447
aoklyunin <aoklyunin@gmail.com>
2017-07-06 15:51:55 +0300

    ьти

---
+++ localCode/VKHelper.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import vk
+
+
+class VKHelper():
+    ALICE_ID = 32897432
+    MISHA_ID = 34499244
+    MY_ID = 303154598
+
+    def __init__(self, psw):
+        self.session = vk.AuthSession('5229876', 'aoklyunin@yandex.ru', psw, scope='wall, messages')
+        self.vk_api = vk.API(self.session, v='5.35', lang='ru')
+
+    def wallPost(self, s):
+        self.vk_api.wall.post(message=s)
+
+    def sendMessage(self, s, u_id):
+        self.vk_api.messages.send(user_id=u_id, message=s)
---
+++ plan/management/commands/mishaSend.py
@@ -0,0 +1,38 @@
+# coding=utf-8
+# программа для загрузки оценок
+import datetime
+
+import itertools
+from django.contrib.auth.models import User
+from django.core.management import BaseCommand
+
+# описание класса програмы
+from localCode.VKHelper import VKHelper
+
+
+class Command(BaseCommand):
+    # описание программы
+    helf = 'mishaSend'
+
+    def getWord(self, a):
+        if a != 11 and a != 12 and a != 13 and a != 13 and a != 14:
+            if a % 10 == 1:
+                return "день"
+            elif a % 10 == 2 or a % 10 == 3 or a % 10 == 4:
+                return "дня"
+        return "дней"
+
+    def getDays(self):
+        a = '2017-08-20'.split('-')
+        aa = datetime.date(int(a[0]), int(a[1]), int(a[2]))
+        bb = datetime.date.today()
+        s = str(aa - bb).split()[0]
+        return s + " " + self.getWord(int(s))
+
+    def add_arguments(self, parser):
+        parser.add_argument('pass', nargs='+', type=str)
+
+    def handle(self, *args, **options):
+        vkh = VKHelper(options['pass'][1])
+        vkh.sendMessage("Осталось хуй да нихуя: "+self.getDays()+".\n Пора вкалывать)", VKHelper.MISHA_ID)
+
